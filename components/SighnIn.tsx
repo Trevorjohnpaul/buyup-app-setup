8b942a9a57d7aa942f882a4cccc2bd5d85b648ad
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2026-02-13 10:59:07 +0300

    feat:navigation menu

---
+++ components/SighnIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const SighnIn = () => {
+  return (
+   <Button className='text-sm font-semibold hover:text-lightOrange'>Login</Button>
+  )
+}
+
+export default SighnIn
