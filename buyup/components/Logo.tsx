103d68820d5575670e5b7a6b2489ad8b3d769ef8
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2026-02-14 19:13:44 +0300

    feat:correction on app folder files

--- buyup/components/Logo.tsx
+++ buyup/components/Logo.tsx
@@ -2,12 +2,24 @@ import Link from 'next/link'
 import React from 'react'
 import { cn } from '@/lib/utils'
 
-const Logo = ({ className }: { className?: string }) => {
-  return <Link href={"/"} className='inline-flex'>
-   <h2 className={cn("text-2xl text-shop-lightblue font-black tracking-wider   hover:text-shop_orange", className)}>
-    Buy<span className='text-shop_orange hover:text-shop-lightblue'>Up</span>
-    </h2>
-  </Link>
-};
+interface LogoProps {
+  className?: string
+}
+
+const Logo: React.FC<LogoProps> = ({ className }) => {
+  return (
+    <Link href="/" className="inline-flex">
+      <h2
+        className={cn(
+          "text-2xl text-shop-lightblue font-black tracking-wider hover:text-shop_orange",
+          className
+        )}
+      >
+        Buy
+        <span className="text-shop_orange hover:text-shop-lightblue">Up</span>
+      </h2>
+    </Link>
+  )
+}
 
 export default Logo
---
+++ buyup/proxy.ts
@@ -0,0 +1,21 @@
+// import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
+
+// /proxy.ts
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ['/((?!_next|.*\\..*).*)'], // matches all routes except Next.js internals and static files
+};
